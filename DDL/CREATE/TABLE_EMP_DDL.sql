55da26b8e367e5d2ebcd0bf2acac6582fcaac04d
Elelero <91528640+Elelero@users.noreply.github.com>
2022-10-13 15:20:28 +0900

    모든 열의 각 자료형을 정의해서 테이블 생성

---
+++ DDL/CREATE/TABLE_EMP_DDL.sql
@@ -0,0 +1,10 @@
+create table emp_ddl (
+  empno number(4),
+  ename varchar2(10),
+  job varchar2(9),
+  mgr number(4),
+  hiredate date,
+  sal number(7, 2),
+  comm number(7, 2),
+  deptno number(2)
+);
